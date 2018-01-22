449b0e5a44477f3707ba5d054df13bcd004cc7b1
HoangNamk64b <namnguyenhoang1996@gmail.com>
2018-01-22 22:26:20 +0700

    controler
    test

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,8 +5,14 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-
+		$this->view->disable();
+	echo "hello index action";		
     }
-
+	public function detailAction()
+	
+	{
+			$this->view->disalbe();
+			echo"hello I detail Action";
+	}
 }
 
